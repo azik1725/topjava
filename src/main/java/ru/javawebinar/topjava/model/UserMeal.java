4c8c5184f53b70eace8ce1e8d6f6fb74e1aeda23
azik1725 <shakhverdievazer@rambler.ru>
2015-12-04 07:40:39 +0300

    Prepare to start project

--- src/main/java/ru/javawebinar/topjava/model/UserMeal.java
+++ src/main/java/ru/javawebinar/topjava/model/UserMeal.java
@@ -2,9 +2,15 @@ package ru.javawebinar.topjava.model;
 
 import java.time.LocalDateTime;
 
+/**
+ * GKislin
+ * 11.01.2015.
+ */
 public class UserMeal {
     protected final LocalDateTime dateTime;
+
     protected final String description;
+
     protected final int calories;
 
     public UserMeal(LocalDateTime dateTime, String description, int calories) {
